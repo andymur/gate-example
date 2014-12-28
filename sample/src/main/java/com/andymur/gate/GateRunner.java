00abbb081a7ff154f3e4b680a1e7075ea2b5aacf
andymur <andymur@lenovo.murhome>
2014-12-29 02:33:08 +0400

    add help method

--- sample/src/main/java/com/andymur/gate/GateRunner.java
+++ sample/src/main/java/com/andymur/gate/GateRunner.java
@@ -3,15 +3,18 @@ import gate.Gate;
 import gate.Document;
 import gate.util.GateException;
 import gate.Factory;
-import gate.creole.SerialAnalyserController;
 
 import java.util.Iterator;
 import java.io.File;
 
 class GateRunner {
-	public static void main(String [] args) throws GateException {
+	public static void main(String [] args) {
 		System.out.println("trying to init gate...");
-		init();
+		try {
+			init();
+		} catch (Exception e) {
+			System.out.println("initialization error occured!");
+		}
 		System.out.println("initialization completed");
 	}
 	
