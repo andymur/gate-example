11329f1e866a7ec47500d11fea9757d250be893f
andymur <andymur@thinkpad.murhome>
2015-01-02 01:15:50 +0300

    some small changes

--- sample/src/main/java/com/andymur/gate/GateRunner.java
+++ sample/src/main/java/com/andymur/gate/GateRunner.java
@@ -1,11 +1,14 @@
 package com.andymur.gate;
-import gate.Gate;
-import gate.Document;
-import gate.util.GateException;
-import gate.Factory;
 
-import java.util.Iterator;
+import gate.*;
+import gate.creole.ANNIEConstants;
+import gate.creole.ConditionalSerialAnalyserController;
+import gate.creole.ResourceInstantiationException;
+import gate.util.GateException;
+import gate.util.persistence.PersistenceManager;
+
 import java.io.File;
+import java.io.IOException;
 
 class GateRunner {
 	public static void main(String [] args) {
@@ -14,11 +17,33 @@ class GateRunner {
 			init();
 		} catch (Exception e) {
 			System.out.println("initialization error occured!");
+            e.printStackTrace();
 		}
 		System.out.println("initialization completed");
 	}
 	
-	private static void init() throws GateException {
+	private static void init() throws GateException, IOException {
 		Gate.init();
+        ConditionalSerialAnalyserController controller = (ConditionalSerialAnalyserController)
+                PersistenceManager.loadObjectFromFile(new File(new File(
+                        Gate.getPluginsHome(), ANNIEConstants.PLUGIN_DIR),
+                        ANNIEConstants.DEFAULT_FILE
+                ));
 	}
+
+    private static Corpus createCorpusFromDocument(String name, String documentUrl) throws ResourceInstantiationException {
+        Corpus corpus = Factory.newCorpus(name);
+        Document gateDocument = createDocumentByUrl(documentUrl);
+        corpus.add(gateDocument);
+        return corpus;
+    }
+
+    private static Document createDocumentByUrl(String url) throws ResourceInstantiationException {
+        FeatureMap params = Factory.newFeatureMap();
+        params.put("sourceUrl", url);
+        params.put("preserveOriginalContent", new Boolean(true));
+        params.put("collectRepositioningInfo", new Boolean(true));
+        return (Document) Factory.createResource(gate.corpora.DocumentContentImpl.class.getCanonicalName(),
+                params);
+    }
 }
