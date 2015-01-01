f80f778bff3d4e5be136ba4c8177ef8ddf607227
andymur <andymurh@gmail.com>
2015-01-02 03:56:26 +0400

    some changes

--- sample/src/main/java/com/andymur/gate/GateRunner.java
+++ sample/src/main/java/com/andymur/gate/GateRunner.java
@@ -3,9 +3,14 @@ import gate.Gate;
 import gate.Document;
 import gate.util.GateException;
 import gate.Factory;
+import gate.creole.*;
+import gate.util.persistence.*;
+import gate.creole.ANNIEConstants;
+import gate.util.persistence.PersistenceManager;
 
 import java.util.Iterator;
 import java.io.File;
+import java.io.*;
 
 class GateRunner {
 	public static void main(String [] args) {
@@ -14,11 +19,16 @@ class GateRunner {
 			init();
 		} catch (Exception e) {
 			System.out.println("initialization error occured!");
+			System.out.println(e);
+			e.printStackTrace();
 		}
 		System.out.println("initialization completed");
 	}
 	
-	private static void init() throws GateException {
+	private static void init() throws GateException, IOException {
 		Gate.init();
+		PersistenceManager p;
+		File f = new File(new File(Gate.getPluginsHome(), ANNIEConstants.PLUGIN_DIR), ANNIEConstants.DEFAULT_FILE);
+		ConditionalSerialAnalyserController controller = (ConditionalSerialAnalyserController) PersistenceManager.loadObjectFromFile(f);
 	}
 }
